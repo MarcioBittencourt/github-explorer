d556bc7194e00f26f06889cc92e8d4e5efe9c1ce
Marcio Bittencourt <marciobitt97@gmail.com>
2022-01-28 15:24:00 -0300

    accessing parameter by route path

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,21 +1,10 @@
-import { PathMatch, PathPattern } from "react-router";
-import { useLocation, useParams } from "react-router-dom"
-
-/* declare function useMatch<ParamKey extends string = string>(
-  pattern: PathPattern | string
-): PathMatch<ParamKey> | null; */
+import { useParams } from "react-router-dom"
 
 const Repository: React.FC = () => {
-  /* const location = useLocation(); */
-
-  /* const params = useMatch(location.pathname); */
   const params = useParams();
-  console.log("teste");
-  console.log(params);
 
   return (
     <>
-      <h1>Repository:{/* params?.pathname */}</h1>
       <h1>Repository:{ params }</h1>
     </>
   );
